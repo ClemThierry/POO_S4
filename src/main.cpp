473a1e7db4a87fcd4a1f11ae4c39db916b3f4fcc
Clemence <clemence.thierry60@orange.fr>
2022-02-23 16:58:57 +0100

    Guess the number
    -pickRandomNumber does't work as well as it could
    -the global game is good

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
+#include <stdlib.h>
 #include <iostream>
+
+int pick_random_number()
+{
+    return rand() % 10 + 1;
+};
+
+void compare_user_number_to_random_number(int theNumberToGuess, int theUserNumber)
+{
+    if (theUserNumber > theNumberToGuess) {
+        std::cout << "Smaller" << std::endl;
+    }
+    else {
+        std::cout << "Greater" << std::endl;
+    }
+};
+
+int askNumberToTheUser()
+{
+    int number;
+    std::cin >> number;
+    while (number < 0 || number > 100) {
+        std::cout << "I said between 0 and 100 ... Think !" << std::endl;
+        std::cin >> number;
+    }
+    return number;
+}
 
 int main()
 {
-    std::cout << "Hello World\n";
+    std::cout << "THE GAME" << std::endl;
+    int numberToGuess = pick_random_number();
+
+    std::cout << "I picked a number between 0 and 100" << std::endl
+              << "Guess the number : " << std::endl;
+    int userNumber = askNumberToTheUser();
+
+    compare_user_number_to_random_number(numberToGuess, userNumber);
+
+    while (numberToGuess != userNumber) {
+        userNumber = askNumberToTheUser();
+        compare_user_number_to_random_number(numberToGuess, userNumber);
+    }
+
+    std::cout << "Congrats, you won !" << std::endl;
 }
