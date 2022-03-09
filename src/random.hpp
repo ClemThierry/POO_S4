9ba0d384571cbe8148c2502f5ecb8e0d28635210
Clemence <clemence.thierry60@orange.fr>
2022-03-09 15:46:44 +0100

    Separation between random.cpp and random.hpp

---
+++ src/random.hpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+
+int pick_random_number();
+
+void compare_user_number_to_random_number(int theNumberToGuess, int theUserNumber);
+
+int askNumberToTheUser();
+
+void randomGame();
