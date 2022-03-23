32570af664e06e3b17866b7f0308a0422304532c
Clemence <clemence.thierry60@orange.fr>
2022-03-23 15:14:34 +0100

    Upgrade the menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -3,18 +3,27 @@
 
 void chooseGame()
 {
+    std::vector<char> possibleOption = {'0', '1', 'q'};
+
     std::cout << "Welcome !!!" << std::endl;
     std::cout << "The available games are : \n -The hangman = 0 \n -The random game = 1" << std::endl;
     std::cout << "With which game do you want to play ?" << std::endl;
 
-    unsigned int numberOfGameChoose;
+    char numberOfGameChoose;
     std::cin >> numberOfGameChoose;
 
+    while (std::none_of(possibleOption.begin(), possibleOption.end(), compare(numberOfGameChoose))) {
+        std::cout << "CHOOSE A CORRECT OPTION !" << std::endl;
+        std::cout << "The available games are : \n -The hangman = 0 \n -The random game = 1" << std::endl;
+        std::cout << "With which game do you want to play ?" << std::endl;
+        std::cin >> numberOfGameChoose;
+    }
+
     switch (numberOfGameChoose) {
-    case 0:
+    case '0':
         hangmanGame();
         break;
-    case 1:
+    case '1':
         randomGame();
         break;
     default:
--- src/menu.hpp
+++ src/menu.hpp
@@ -1,6 +1,18 @@
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 #include "Hangman.hpp"
 #include "random.hpp"
+
+struct compare {
+    int key;
+    compare(int const& i)
+        : key(i) {}
+
+    bool operator()(int const& i)
+    {
+        return (i == key);
+    }
+};
 
 void chooseGame();
