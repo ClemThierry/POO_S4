04e33ee966d832803b1d9f32cc5571b5ac74a4ef
Clemence <clemence.thierry60@orange.fr>
2022-04-13 15:37:20 +0200

    Update colors and add event click

--- src/NoughtsAndCrosses.cpp
+++ src/NoughtsAndCrosses.cpp
@@ -5,16 +5,20 @@ void noughtsCrossesGame()
 {
     auto  ctx      = p6::Context{{900, 900, "Noughts and Crosses"}};
     float sizeBoxe = ctx.aspect_ratio() / 3;
-    std::cout << sizeBoxe;
-    ctx.update = [&]() {                    // Define the function that will be called in a loop once you call ctx.start()
-        ctx.background({0.2f, 0.1f, 0.3f}); // Clear the background with some color (Try to comment out this line to see what happens)
-        ctx.fill = {0, 1, 0, 0.5};
+    std::cout << sizeBoxe << std::endl;
+    ctx.update = [&]() {                  // Define the function that will be called in a loop once you call ctx.start()
+        ctx.background({0.f, 1.f, 0.7f}); // Clear the background with some color (Try to comment out this line to see what happens)
+        ctx.fill = {0, 0, 0, 0};
         for (float x = -1; x < 1; x += 2 * sizeBoxe) {
             for (float y = 1; y > -1; y -= 2 * sizeBoxe) {
                 ctx.square(p6::TopLeftCorner{x, y}, p6::Radius{sizeBoxe});
             }
         }
 
+    };
+
+    ctx.mouse_pressed = [&](p6::MouseButton event) {
+        std::cout << "sizeBoxe";
     };
     ctx.start();
 }
